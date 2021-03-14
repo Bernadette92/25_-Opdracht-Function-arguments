ec94308d249a44a744a5b9aaaedef7685dec36a2
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-03-14 17:09:38 +0100

    Add files via upload

---
+++ 25_ Opdracht-Function-arguments/script.js
@@ -0,0 +1,14 @@
+const paintWall = function (color) {
+  console.log("The wall has been painted " + color);
+};
+
+paintWall("blue");
+
+const paintSinlgeWall = function (colorB, colorA) {
+  console.log(colorB, colorA);
+};
+
+paintSinlgeWall(
+  "The south-east wall has been painted grey",
+  "The north wall has been painted orange"
+);
